5818102646cceaebc637f3ce014c2171c080e449
Najat <craazylolla@gmail.com>
2023-11-11 13:44:40 -0800

    m

--- console.py
+++ console.py
@@ -129,5 +129,17 @@ class HBNBCommand(cmd.Cmd):
             else:
                 print('** no instance found **')
 
+    def do_count(self, line):
+        """Display count of instances specified"""
+        if line in HBNBCommand.classes:
+            count = 0
+            for key, objs in storage.all().items():
+                if line in key:
+                    count += 1
+            print(count)
+        else:
+            print("** class doesn't exist **")
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
