54d635309c4bfdaeee4b5b29c51e27ef0d6b9fd5
Najat <craazylolla@gmail.com>
2023-11-11 11:48:11 -0800

    m

--- models/base_model.py
+++ models/base_model.py
@@ -13,11 +13,17 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Constructor"""
         if kwargs:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    setattr(self, k, datetime.strptime(v, self.format_str))
-                elif k != "__class__":
-                    setattr(self, k, v)
+            for key, val in kwargs.items():
+                if "created_at" == key:
+                    self.created_at = datetime.strptime(kwargs["created_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif "updated_at" == key:
+                    self.updated_at = datetime.strptime(kwargs["updated_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif "__class__" == key:
+                    pass
+                else:
+                    setattr(self, key, val)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
