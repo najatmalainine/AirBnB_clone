3403dafaa65fcb78e2df79c07b4ee8e2cd63c044
Najat <craazylolla@gmail.com>
2023-11-11 12:23:51 -0800

    m

--- models/base_model.py
+++ models/base_model.py
@@ -22,6 +22,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """returns [<class name>] (<self.id>) <self.__dict__>"""
@@ -31,6 +32,7 @@ class BaseModel:
     def save(self):
         """updates updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
